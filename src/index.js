d3e1aed872afe533bf228a6623d17525b0439f88
saifilwebisoft <saifilmomin4@gmail.com>
2022-07-31 02:44:17 -0400

    Added leaving page prompt

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+window.onbeforeunload = function() {
+  return true;
+};
+
 root.render(
   <React.StrictMode>
     <App />
